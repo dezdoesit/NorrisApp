9e6002c96e61762a98f69d2fa29d28912eb1de04
dezdoesit <dez.blair06@gmail.com>
2024-04-27 09:26:18 -0400

    Initial Commit

---
+++ iAmAJokeApp/iAmAJokeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  iAmAJokeAppApp.swift
+//  iAmAJokeApp
+//
+//  Created by dmoney on 4/27/24.
+//
+
+import SwiftUI
+
+@main
+struct iAmAJokeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
